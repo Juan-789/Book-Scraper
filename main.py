4d0fc0e090b7f9828572e103a71e7c03b780613b
Juan-789 <juan.marulanda@stu.ocsb.ca>
2024-01-26 15:25:45 -0500

    Made conditions to remove garbage links from libgen

--- main.py
+++ main.py
@@ -8,19 +8,27 @@ URL: str = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l13
 URL1: str = f'https://www.amazon.ca/s?k={query}&ref=nb_sb_noss'
 URL2: str = f'https://www.biblio.com/search.php?stage=1&keyisbn={query}'
 URL3: str = f'https://www.libgen.is/search.php?req={query}&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def'
-urls = [URL, URL1, URL2, URL3]
+urls = [ URL1,  URL3,URL2]
+file = open('ItemLinks.html', 'w')
+queryLinks = []
+
+
 for url in urls:
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     for link in soup.find_all('a'):
         l = str(link.get('href'))
-        if ('ads' in l ) or 'http' not in l:
+        if ('ads' in l ) or ('http' not in l) or ('library.bz'  in l) or ('mhut'  in l) or ('onion'  in l):
+            continue
+        elif (url == URL3) and(( 'phillm'  in l) or ('magzdb'  in l)):
             continue
         else:
-            print(link.get('href'))
+            queryLinks.append(link)
 
     print(url, r.status_code)
 
+
+file.write(str(queryLinks))
 
 # <a class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal" href="/sspa/click?ie=UTF8&amp;spc=MTo2Njg1NDgxODg1NTUzMDI0OjE3MDYxNTE4MzA6c3BfYXRmOjMwMDA0MzQ0NzkyMTcwMjo6MDo6&amp;url=%2FLEGO-Star-Wars-Chancellor-Minifigures%2Fdp%2FB0BXQ5SX82%2Fref%3Dsr_1_3_sspa%3Fcrid%3DKURZDJBAA7DI%26keywords%3Dclone%2Bwars%26qid%3D1706151830%26sprefix%3Dclone%2Bwars%252Caps%252C103%26sr%3D8-3-spons%26sp_csd%3Dd2lkZ2V0TmFtZT1zcF9hdGY%26psc%3D1"><span class="a-size-base-plus a-color-base a-text-normal"
